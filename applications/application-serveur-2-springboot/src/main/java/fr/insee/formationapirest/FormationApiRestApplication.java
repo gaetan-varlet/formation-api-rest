75db0f97a9853b738d1b6c9db87acd8de076f818
Gaëtan Varlet <gaetan.varlet@insee.fr>
2019-02-06 13:06:51 +0100

    maj création du ZIP pour la prod du CEI

--- applications/application-serveur-2-springboot/src/main/java/fr/insee/formationapirest/FormationApiRestApplication.java
+++ applications/application-serveur-2-springboot/src/main/java/fr/insee/formationapirest/FormationApiRestApplication.java
@@ -2,12 +2,20 @@ package fr.insee.formationapirest;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 
 @SpringBootApplication
 public class FormationApiRestApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(FormationApiRestApplication.class, args);
+	}
+	
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {		
+		// spring.config.location permet de définir les chemins où spring va chercher des fichiers de properties pour la prod pour le CEI
+		return application.properties(
+				"spring.config.location=classpath:/,file:///${catalina.base}/webapps/formation.properties"
+				).sources(FormationApiRestApplication.class);
 	}
 
 }
