be838aa493c1bddae41bca329f36d138340c8a6e
Gaëtan Varlet <gaetan.varlet@insee.fr>
2019-04-24 10:58:04 +0200

    suppression d'un import inutile

--- applications/application-serveur-2-springboot/src/main/java/fr/insee/formationapirest/model/Vin.java
+++ applications/application-serveur-2-springboot/src/main/java/fr/insee/formationapirest/model/Vin.java
@@ -1,7 +1,5 @@
 package fr.insee.formationapirest.model;
 
-import java.util.Optional;
-
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
