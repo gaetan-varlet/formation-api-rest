6d58d753879f74a544df082a16d920991b27ca94
Gaëtan Varlet <gaetanvarlet@gmail.com>
2019-04-26 08:35:06 +0200

    maj partie cache

--- applications/application-serveur-2-springboot/src/main/java/fr/insee/formationapirest/service/VinService.java
+++ applications/application-serveur-2-springboot/src/main/java/fr/insee/formationapirest/service/VinService.java
@@ -6,16 +6,18 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
+import com.opencsv.CSVWriter;
+import com.opencsv.CSVWriterBuilder;
+import com.opencsv.ICSVWriter;
+import com.querydsl.core.types.Predicate;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cache.annotation.Cacheable;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
-
-import com.opencsv.CSVWriter;
-import com.opencsv.CSVWriterBuilder;
-import com.opencsv.ICSVWriter;
-import com.querydsl.core.types.Predicate;
 
 import fr.insee.formationapirest.exception.VinInconnuException;
 import fr.insee.formationapirest.exception.VinInvalideException;
@@ -24,68 +26,71 @@ import fr.insee.formationapirest.repository.VinRepository;
 
 @Service
 public class VinService {
-	
+
+	private static final Logger log = LoggerFactory.getLogger(VinService.class);
+
 	@Autowired
 	VinRepository vinRepository;
-	
-	public List<Vin> findAll(String appellation){
+
+	public List<Vin> findAll(String appellation) {
 		Optional<String> optional = Optional.ofNullable(appellation);
-		if(optional.isPresent()) {
+		if (optional.isPresent()) {
 			return vinRepository.findByAppellation(appellation);
 		} else {
 			return vinRepository.findAll();
 		}
 	}
-	
-	public Iterable<Vin> get(Predicate predicate){
+
+	public Iterable<Vin> get(Predicate predicate) {
 		return vinRepository.findAll(predicate);
 	}
-	
-	public Vin getById(Integer id){
-		if(id>0) {
-			if(vinRepository.existsById(id)) {
+
+	public Vin getById(Integer id) {
+		if (id > 0) {
+			if (vinRepository.existsById(id)) {
 				return vinRepository.findById(id).orElse(null);
 			} else {
-				throw new VinInconnuException("le vin avec l'id "+ id + " n'existe pas");
+				throw new VinInconnuException("le vin avec l'id " + id + " n'existe pas");
 			}
 		} else {
 			throw new VinInvalideException("l'id renseigné (" + id + ") n'est pas valide");
 		}
 	}
-	
-	public void deleteById(Integer id){
-		if(vinRepository.existsById(id)) {
+
+	public void deleteById(Integer id) {
+		if (vinRepository.existsById(id)) {
 			vinRepository.deleteById(id);
 		} else {
-			throw new VinInconnuException("le vin avec l'id "+ id + " n'existe pas");
+			throw new VinInconnuException("le vin avec l'id " + id + " n'existe pas");
 		}
 	}
-	
-	public Vin add(Vin vin){
-		if(controleValiditeVin(vin)) {
+
+	public Vin add(Vin vin) {
+		if (controleValiditeVin(vin)) {
 			// si l'id n'est pas renseigné ou si l'id renseigné n'existe pas, alors on crée le vin
-			if(vin.getId() == null || !vinRepository.existsById(vin.getId())){
+			if (vin.getId() == null || !vinRepository.existsById(vin.getId())) {
 				return vinRepository.save(vin);
 			} else {
-				throw new VinInvalideException("le vin renseigné (" + vin + ") existe déjà, vous ne pouvez pas le créer");
+				throw new VinInvalideException(
+						"le vin renseigné (" + vin + ") existe déjà, vous ne pouvez pas le créer");
 			}
 		} else {
 			throw new VinInvalideException("le vin renseigné (" + vin + ") n'est pas valide");
 		}
 	}
-	
-	public Vin update(Vin vin){
-		if(vinRepository.existsById(vin.getId())) {
+
+	public Vin update(Vin vin) {
+		if (vinRepository.existsById(vin.getId())) {
 			return vinRepository.save(vin);
 		} else {
-			throw new VinInconnuException("le vin avec l'id "+ vin.getId() + " n'existe pas");	
+			throw new VinInconnuException("le vin avec l'id " + vin.getId() + " n'existe pas");
 		}
 	}
-	
+
 	public Page<Vin> pageable(Pageable p) {
 		return vinRepository.findAll(p);
 	}
-	
+
 	protected boolean controleValiditeVin(Vin vin) {
 		return Optional.ofNullable(vin)
 			.filter(this::isChateauValid)
@@ -111,14 +116,14 @@ public class VinService {
 			.filter(p -> p >= 0)
 			.isPresent();
 	}
-	
+
 	public void ecrireVinsDansCsv(Writer writer, List<Vin> listeAEcrire) throws IOException {
 		// Création du writer openCSV qui va écrire dans le writer fourni en paramètre
 		ICSVWriter csvWriter = new CSVWriterBuilder(writer)
-				.withSeparator(';') // séparateur point-virgule (virgule par défaut)
+			.withSeparator(';') // séparateur point-virgule (virgule par défaut)
 				.withQuoteChar(CSVWriter.DEFAULT_QUOTE_CHARACTER) // caractère autour de chaque attribut (doubles quotes par défaut)
 				.build();
-		
+
 		listeAEcrire.forEach(vin -> {
 			List<String> ligne = new ArrayList<>();
 			ligne.add(vin.getChateau());
@@ -130,9 +135,10 @@ public class VinService {
 	}
 
 	@Cacheable("longService")
-	public boolean longService(int i) throws Exception{
+	public boolean longService(int i) throws Exception {
+		log.info("passage dans la méthode longService()");
 		Thread.sleep(2000);
 		return i % 2 == 0 ? true : false;
 	}
-	
+
 }
