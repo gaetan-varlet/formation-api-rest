b1586458d9119a8781e0d1ae714a671f9cfdabcb
Gaëtan Varlet <gaetanvarlet@gmail.com>
2020-07-07 07:19:03 +0200

    passage à junit5

--- applications/application-serveur-2-springboot/src/test/java/fr/insee/formationapirest/controller/TestControllerIntegrationTest.java
+++ applications/application-serveur-2-springboot/src/test/java/fr/insee/formationapirest/controller/TestControllerIntegrationTest.java
@@ -9,120 +9,95 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 import java.io.InputStream;
 
-import org.junit.Test;
-import org.junit.runner.RunWith;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.mock.web.MockMultipartFile;
 import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.context.TestPropertySource;
-import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
 
-@RunWith(SpringRunner.class)
-@SpringBootTest(webEnvironment=SpringBootTest.WebEnvironment.RANDOM_PORT)
+@ExtendWith(SpringExtension.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @AutoConfigureMockMvc
 @TestPropertySource(locations = "classpath:formation-api-rest.properties")
 public class TestControllerIntegrationTest {
-	
+
 	@Autowired
 	private MockMvc mvc;
-	
+
 	@Test
-	public void DoitRecupererNomPasAuthentifie() throws Exception{
-		mvc.perform(get("/mon-nom"))
-		.andExpect(status().isUnauthorized())
-		;
+	public void DoitRecupererNomPasAuthentifie() throws Exception {
+		mvc.perform(get("/mon-nom")).andExpect(status().isUnauthorized());
 	}
-	
+
 	@Test
 	@WithMockUser
-	public void DoitRecupererNom() throws Exception{
-		mvc.perform(get("/mon-nom"))
-		.andExpect(status().isOk())
-		.andExpect(content().string("Gaetan"));
+	public void DoitRecupererNom() throws Exception {
+		mvc.perform(get("/mon-nom")).andExpect(status().isOk()).andExpect(content().string("Gaetan"));
 		;
 	}
-	
+
 	@Test
-	@WithMockUser(roles="ADMIN_TOUCAN")
-	public void DoitRecupererEnvironnement() throws Exception{
-		mvc.perform(get("/environnement"))
-		.andExpect(status().isOk())
-		.andExpect(jsonPath("$",is("environnement local")))
-		;
+	@WithMockUser(roles = "ADMIN_TOUCAN")
+	public void DoitRecupererEnvironnement() throws Exception {
+		mvc.perform(get("/environnement")).andExpect(status().isOk())
+				.andExpect(jsonPath("$", is("environnement local")));
 	}
-	
+
 	@Test
-	public void DoitRecupererEnvironnementPasAuthentifie() throws Exception{
-		mvc.perform(get("/environnement"))
-		.andExpect(status().isUnauthorized())
-		;
+	public void DoitRecupererEnvironnementPasAuthentifie() throws Exception {
+		mvc.perform(get("/environnement")).andExpect(status().isUnauthorized());
 	}
-	
+
 	@Test
-	@WithMockUser(roles="MAUVAIS_ROLE")
-	public void DoitRecupererEnvironnementRoleInvalide() throws Exception{
-		mvc.perform(get("/environnement"))
-		.andExpect(status().isForbidden())
-		;
+	@WithMockUser(roles = "MAUVAIS_ROLE")
+	public void DoitRecupererEnvironnementRoleInvalide() throws Exception {
+		mvc.perform(get("/environnement")).andExpect(status().isForbidden());
 	}
-	
+
 	@Test
-	public void uploadFichier() throws Exception{
-		MockMultipartFile firstFile = new MockMultipartFile("multipartfile", "filename.txt", "text/plain", "coucou".getBytes());
-		
-		mvc.perform(multipart("/upload")
-				.file(firstFile))
-		.andExpect(status().isOk())
-		.andExpect(content().string("coucou"));
+	public void uploadFichier() throws Exception {
+		MockMultipartFile firstFile = new MockMultipartFile("multipartfile", "filename.txt", "text/plain",
+				"coucou".getBytes());
+
+		mvc.perform(multipart("/upload").file(firstFile)).andExpect(status().isOk())
+				.andExpect(content().string("coucou"));
 	}
-	
+
 	@Test
-	public void uploadFichier2() throws Exception{
+	public void uploadFichier2() throws Exception {
 
 		InputStream inputSteam = getClass().getClassLoader().getResourceAsStream("toto.txt");
 		MockMultipartFile multipartFile = new MockMultipartFile("multipartfile", inputSteam);
-		
-		mvc.perform(multipart("/upload")
-				.file(multipartFile))
-		.andExpect(status().isOk())
-		.andExpect(content().string("Je m'appelle Toto"));
+
+		mvc.perform(multipart("/upload").file(multipartFile)).andExpect(status().isOk())
+				.andExpect(content().string("Je m'appelle Toto"));
 	}
-	
+
 	@Test
 	public void DoitRetournerIdepSansIdep() throws Exception {
-		mvc.perform(get("/principal"))
-		.andExpect(status().isOk())
-		.andExpect(jsonPath("$",is("Mon idep est null.")))
-		;
+		mvc.perform(get("/principal")).andExpect(status().isOk()).andExpect(jsonPath("$", is("Mon idep est null.")));
 	}
-	
+
 	@Test
-	@WithMockUser(username="toto")
+	@WithMockUser(username = "toto")
 	public void DoitRetournerIdepAvecIdep() throws Exception {
-		mvc.perform(get("/principal"))
-		.andExpect(status().isOk())
-		.andExpect(jsonPath("$",is("Mon idep est toto.")))
-		;
+		mvc.perform(get("/principal")).andExpect(status().isOk()).andExpect(jsonPath("$", is("Mon idep est toto.")));
 	}
-	
+
 	@Test
-	@WithMockUser(roles="toto")
+	@WithMockUser(roles = "toto")
 	public void DoitValiderRole() throws Exception {
-		mvc.perform(get("/role/toto"))
-		.andExpect(status().isOk())
-		.andExpect(jsonPath("$",is(true)))
-		;
+		mvc.perform(get("/role/toto")).andExpect(status().isOk()).andExpect(jsonPath("$", is(true)));
 	}
-	
+
 	@Test
 	public void DoitInvaliderRole() throws Exception {
-		mvc.perform(get("/role/toto"))
-		.andExpect(status().isOk())
-		.andExpect(jsonPath("$",is(false)))
-		;
+		mvc.perform(get("/role/toto")).andExpect(status().isOk()).andExpect(jsonPath("$", is(false)));
 	}
-	
+
 }
--- applications/application-serveur-2-springboot/src/test/java/fr/insee/formationapirest/service/VinServiceTest.java
+++ applications/application-serveur-2-springboot/src/test/java/fr/insee/formationapirest/service/VinServiceTest.java
@@ -1,39 +1,52 @@
 package fr.insee.formationapirest.service;
 
-import static org.junit.Assert.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import java.util.Arrays;
 import java.util.List;
 
-import org.junit.Before;
-import org.junit.Test;
-import org.junit.runner.RunWith;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Mockito;
-import org.mockito.junit.MockitoJUnitRunner;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.mockito.junit.jupiter.MockitoSettings;
+import org.mockito.quality.Strictness;
 
 import fr.insee.formationapirest.model.Vin;
 import fr.insee.formationapirest.repository.VinRepository;
 
-@RunWith(MockitoJUnitRunner.class)
+@ExtendWith(MockitoExtension.class)
+@MockitoSettings(strictness = Strictness.LENIENT)
 public class VinServiceTest {
-	
+
 	@InjectMocks
 	private VinService vinService;
-	
+
 	@Mock
 	private VinRepository vinRepository;
-	
-	@Before
+
+	@BeforeEach
 	public void setUp() {
-		Vin vin1 = new Vin(); vin1.setId(1); vin1.setChateau("Château 1"); vin1.setAppellation("Saint-Julien"); vin1.setPrix(10.0);
-		Vin vin2 = new Vin(); vin2.setId(2); vin2.setChateau("Château 2"); vin2.setAppellation("Pomerol"); vin2.setPrix(25.0);
+		Vin vin1 = new Vin();
+		vin1.setId(1);
+		vin1.setChateau("Château 1");
+		vin1.setAppellation("Saint-Julien");
+		vin1.setPrix(10.0);
+		Vin vin2 = new Vin();
+		vin2.setId(2);
+		vin2.setChateau("Château 2");
+		vin2.setAppellation("Pomerol");
+		vin2.setPrix(25.0);
 		List<Vin> liste = Arrays.asList(vin1, vin2);
-		
+
 		Mockito.when(vinRepository.findAll()).thenReturn(liste);
 	}
-	
+
 	@Test
 	public void recupererTousLesVins() {
 		List<Vin> vinRetournes = vinService.findAll(null);
@@ -42,22 +55,26 @@ public class VinServiceTest {
 	}
 
 	@Test
-	public void controleValiditeVinTest(){
+	public void controleValiditeVinTest() {
 		assertFalse(vinService.controleValiditeVin(null));
 		assertTrue(vinService.controleValiditeVin(new Vin(1, "Château Lascombes", "Margaux", 100d)));
 		assertTrue(vinService.controleValiditeVin(new Vin(null, "Château Lascombes", "Margaux", 100d)));
-		
+
 		assertFalse(vinService.controleValiditeVin(new Vin(1, null, "Margaux", 100d)));
 		assertFalse(vinService.controleValiditeVin(new Vin(1, "", "Margaux", 100d)));
 		assertTrue(vinService.controleValiditeVin(new Vin(1, "C", "Margaux", 100d)));
-		assertTrue(vinService.controleValiditeVin(new Vin(1, "12345678901234567890123456789012345678901234567890", "Margaux", 100d)));
-		assertFalse(vinService.controleValiditeVin(new Vin(1, "123456789012345678901234567890123456789012345678901", "Margaux", 100d)));
+		assertTrue(vinService.controleValiditeVin(
+				new Vin(1, "12345678901234567890123456789012345678901234567890", "Margaux", 100d)));
+		assertFalse(vinService.controleValiditeVin(
+				new Vin(1, "123456789012345678901234567890123456789012345678901", "Margaux", 100d)));
 
 		assertFalse(vinService.controleValiditeVin(new Vin(1, "Château Lascombes", null, 100d)));
 		assertFalse(vinService.controleValiditeVin(new Vin(1, "Château Lascombes", "", 100d)));
 		assertTrue(vinService.controleValiditeVin(new Vin(1, "Château Lascombes", "M", 100d)));
-		assertTrue(vinService.controleValiditeVin(new Vin(1, "Château Lascombes", "12345678901234567890123456789012345678901234567890", 100d)));
-		assertFalse(vinService.controleValiditeVin(new Vin(1, "Château Lascombes", "123456789012345678901234567890123456789012345678901", 100d)));
+		assertTrue(vinService.controleValiditeVin(
+				new Vin(1, "Château Lascombes", "12345678901234567890123456789012345678901234567890", 100d)));
+		assertFalse(vinService.controleValiditeVin(
+				new Vin(1, "Château Lascombes", "123456789012345678901234567890123456789012345678901", 100d)));
 
 		assertFalse(vinService.controleValiditeVin(new Vin(1, "Château Lascombes", "Margaux", null)));
 		assertFalse(vinService.controleValiditeVin(new Vin(1, "Château Lascombes", "Margaux", -5d)));
@@ -65,5 +82,5 @@ public class VinServiceTest {
 		assertTrue(vinService.controleValiditeVin(new Vin(1, "Château Lascombes", "Margaux", 0d)));
 		assertTrue(vinService.controleValiditeVin(new Vin(1, "Château Lascombes", "Margaux", 0.1d)));
 	}
-	
+
 }
