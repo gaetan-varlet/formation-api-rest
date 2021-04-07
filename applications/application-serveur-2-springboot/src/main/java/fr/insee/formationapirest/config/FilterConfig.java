500c2751f9b47dd4bccaf56457f641d8a5ff2da0
Gaëtan Varlet <gaetanvarlet@gmail.com>
2021-04-07 08:21:08 +0200

    montée de version dépendances

--- applications/application-serveur-2-springboot/src/main/java/fr/insee/formationapirest/config/FilterConfig.java
+++ applications/application-serveur-2-springboot/src/main/java/fr/insee/formationapirest/config/FilterConfig.java
@@ -11,46 +11,44 @@ import com.github.ziplet.filter.compression.CompressingFilter;
 
 @Configuration
 public class FilterConfig {
-	
-	
+
 	// Configuration du filtre pour la gestion du CORS
 	@Bean
 	public FilterRegistrationBean<CorsFilter> corsFilter() {
 		CorsConfiguration config = new CorsConfiguration();
-		config.setAllowCredentials(true);
+		// config.setAllowCredentials(true);
 		config.addAllowedOrigin("*");
 		config.addAllowedHeader("*");
 		config.addAllowedMethod("*");
 		config.setMaxAge(3600L);
 		UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
 		source.registerCorsConfiguration("/**", config);
-        FilterRegistrationBean<CorsFilter> registrationBean = new FilterRegistrationBean<>();
-        registrationBean.setFilter(new CorsFilter(source));
-        registrationBean.setOrder(0);
+		FilterRegistrationBean<CorsFilter> registrationBean = new FilterRegistrationBean<>();
+		registrationBean.setFilter(new CorsFilter(source));
+		registrationBean.setOrder(0);
 		return registrationBean;
-	}	
-	
+	}
+
 	// Configuration du filtre de compression de la réponse
 	@Bean
-	public FilterRegistrationBean<CompressingFilter> compressingFilter(){
+	public FilterRegistrationBean<CompressingFilter> compressingFilter() {
 		FilterRegistrationBean<CompressingFilter> registrationBean = new FilterRegistrationBean<>();
 		registrationBean.setFilter(new CompressingFilter());
 		registrationBean.setOrder(1);
 		return registrationBean;
 	}
-//	@Bean
-//	public Filter compressFilter() {
-//	    return new CompressingFilter();
-//	}
-	
+	// @Bean
+	// public Filter compressFilter() {
+	// return new CompressingFilter();
+	// }
 
 	@Bean
-	public FilterRegistrationBean<PartialFilter> loggingFilter(){
-	    FilterRegistrationBean<PartialFilter> registrationBean = new FilterRegistrationBean<>();        
-	    registrationBean.setFilter(new PartialFilter());
-	    registrationBean.addUrlPatterns("/vin/*");
-	    registrationBean.setOrder(3);        
-	    return registrationBean;    
+	public FilterRegistrationBean<PartialFilter> loggingFilter() {
+		FilterRegistrationBean<PartialFilter> registrationBean = new FilterRegistrationBean<>();
+		registrationBean.setFilter(new PartialFilter());
+		registrationBean.addUrlPatterns("/vin/*");
+		registrationBean.setOrder(3);
+		return registrationBean;
 	}
 
 }
