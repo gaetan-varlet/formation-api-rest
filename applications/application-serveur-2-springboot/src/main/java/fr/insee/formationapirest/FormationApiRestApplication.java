88809a6d72d02ecf178bd6c8c9b6c970b32a25bc
Gaëtan Varlet <gaetan.varlet@insee.fr>
2021-12-20 17:37:17 +0100

    passage de log4j2 à logback

--- applications/application-serveur-2-springboot/src/main/java/fr/insee/formationapirest/FormationApiRestApplication.java
+++ applications/application-serveur-2-springboot/src/main/java/fr/insee/formationapirest/FormationApiRestApplication.java
@@ -18,37 +18,34 @@ import org.springframework.web.context.WebApplicationContext;
 @SpringBootApplication
 @EnableCaching
 public class FormationApiRestApplication extends SpringBootServletInitializer {
-	
+
 	private static final String NOM_FICHIER_PROPERTIES = "formation-api-rest";
 
 	public static void main(String[] args) {
 		// définition des system properties pour le local
 		System.setProperty("spring.config.name", NOM_FICHIER_PROPERTIES);
-		// properties pour savoir où s'enregistrent les logs
-		System.setProperty("dossierLog", "./logs");
-		System.setProperty("nomFichierLog", "formation-api-rest");
-		// permet de colorer les logs sous Windows
-		System.setProperty("log4j.skipJansi", "false");
-		// permet de rendre rendre asynchrone toutes les logs sans toucher au fichier de conf log4j2.xml
-		System.setProperty("Log4jContextSelector", "org.apache.logging.log4j.core.async.AsyncLoggerContextSelector");
 		SpringApplication.run(FormationApiRestApplication.class, args);
 	}
-	
+
 	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-		// spring.config.name permet de définir le nom du fichier de properties lu automatiquement par springboot sous src/main/resources
-		// spring.config.location permet de définir les chemins où spring va chercher des fichiers de properties à charger (ceux définis à la fin sont prioritaires)
+		// spring.config.name permet de définir le nom du fichier de properties lu
+		// automatiquement par springboot sous src/main/resources
+		// spring.config.location permet de définir les chemins où spring va chercher
+		// pour des fichiers de properties à charger (ceux définis à la fin sont
+		// prioritaires)
 		return application.properties(
-			"spring.config.location=classpath:/toucan.properties, file:${catalina.base}/webapps/formation.properties",
-			"spring.config.name="+NOM_FICHIER_PROPERTIES // définition de la property pour le fonctionnement sur les plateformes du CEI
-			).sources(FormationApiRestApplication.class);
+				"spring.config.location=classpath:/toucan.properties, file:${catalina.base}/webapps/formation.properties",
+				"spring.config.name=" + NOM_FICHIER_PROPERTIES
+		// définition de la property pour le fonctionnement sur les plateformes du CEI
+		).sources(FormationApiRestApplication.class);
 	}
 
-	
 	@Bean
 	@Scope(scopeName = WebApplicationContext.SCOPE_REQUEST, proxyMode = ScopedProxyMode.TARGET_CLASS)
-	// renvoie le principal mis dans la requête par Keycloak ou un principal avec un "name" null sinon
+	// renvoie le principal mis dans la requête par Keycloak ou un principal avec un
+	// "name" null sinon
 	public Principal getPrincipal(HttpServletRequest httpRequest) {
 		return Optional.ofNullable(httpRequest.getUserPrincipal()).orElse(() -> null);
 	}
-	
+
 }
