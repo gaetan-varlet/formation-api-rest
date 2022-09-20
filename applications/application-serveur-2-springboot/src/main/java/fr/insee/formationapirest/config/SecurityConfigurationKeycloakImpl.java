accfbd7fadf86dd38382df59b6e53348087eeba9
Gaëtan Varlet <gaetanvarlet@gmail.com>
2022-09-20 19:48:00 +0200

    ajout import manquant

--- applications/application-serveur-2-springboot/src/main/java/fr/insee/formationapirest/config/SecurityConfigurationKeycloakImpl.java
+++ applications/application-serveur-2-springboot/src/main/java/fr/insee/formationapirest/config/SecurityConfigurationKeycloakImpl.java
@@ -1,6 +1,7 @@
 package fr.insee.formationapirest.config;
 
 import java.util.Collection;
+import java.util.Collections;
 import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
