82c6b65ed475af8d60b9363c7a8754bd4bed9669
Gaëtan Varlet <gaetan.varlet@insee.fr>
2023-11-24 18:28:18 +0100

    maj filtre CORS

--- applications/application-serveur-2-springboot/src/main/java/fr/insee/formationapirest/config/FilterConfig.java
+++ applications/application-serveur-2-springboot/src/main/java/fr/insee/formationapirest/config/FilterConfig.java
@@ -2,6 +2,7 @@ package fr.insee.formationapirest.config;
 
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -9,20 +10,27 @@ import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
+import lombok.extern.slf4j.Slf4j;
+
 @Configuration
+@Slf4j
 public class FilterConfig {
+
+	@Value("${fr.insee.cors.authorized.urls}")
+	private String[] urls;
 
 	@Bean
 	public CorsConfigurationSource corsConfigurationSource() {
-        CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(List.of("*"));
-        configuration.setAllowedMethods(List.of("*"));
-        configuration.setAllowedHeaders(List.of("*"));
-        configuration.setMaxAge(3600L);
-        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-        source.registerCorsConfiguration("/**", configuration);
-        return source;
-    }
+		log.info("URLs autorisées pour faire des requêtes HTTP depuis le navigateur : {}", List.of(urls));
+		CorsConfiguration configuration = new CorsConfiguration();
+		configuration.setAllowedOriginPatterns(List.of(urls));
+		configuration.setAllowedMethods(List.of("*"));
+		configuration.setAllowedHeaders(List.of("*"));
+		configuration.setMaxAge(3600L);
+		UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+		source.registerCorsConfiguration("/**", configuration);
+		return source;
+	}
 
 	@Bean
 	public FilterRegistrationBean<PartialFilter> loggingFilter() {
