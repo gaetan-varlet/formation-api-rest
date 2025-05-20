69bc584db42580195a790842bc0c771fd54e92d5
Gaëtan Varlet <gaetan.varlet@insee.fr>
2025-05-20 09:55:59 +0200

    correction pour l'upload de fichier

--- applications/application-serveur-2-springboot/src/main/java/fr/insee/formationapirest/controller/TestController.java
+++ applications/application-serveur-2-springboot/src/main/java/fr/insee/formationapirest/controller/TestController.java
@@ -9,6 +9,7 @@ import jakarta.servlet.http.HttpServletRequest;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -49,7 +50,7 @@ public class TestController {
 		return "Hello World sécurisé !";
 	}
 
-	@PostMapping("/upload")
+	@PostMapping(value = "/upload", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
 	public String upload(@RequestParam MultipartFile multipartfile) throws IOException {
 		return new String(multipartfile.getBytes());
 	}
